2af15a93d6f7d72a8dfa342e0b2370874d0497c4
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-19 22:15:07 +0200

    Update index.js

--- JavaScript/lesson2/task4/index.js
+++ JavaScript/lesson2/task4/index.js
@@ -2,10 +2,9 @@ const a = 4;
 const b = 2;
 console.log('a: ', a);
 console.log('b: ', 2);
-console.log(a - b);
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
-
+console.log('a-b=',a - b);
+console.log('a+b=',a + b);
+console.log('a/b=',a / b);
+console.log('a*b=',a * b);
 
 
