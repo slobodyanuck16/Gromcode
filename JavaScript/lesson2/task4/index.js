574e7f490d3a882ba8dd2ffe5e9087d8b170c87f
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-19 22:12:20 +0200

    commit

---
+++ JavaScript/lesson2/task4/index.js
@@ -0,0 +1,11 @@
+const a = 4;
+const b = 2;
+console.log('a: ', a);
+console.log('b: ', 2);
+console.log('a - b =',a - b);
+console.log('a + b =',a + b);
+console.log('a / b =',a / b);
+console.log('a * b =',a * b);
+
+
+
