d7937c66edc6c8b5c0eda303564e65584a778712
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-20 18:39:12 +0200

    commit

--- JavaScript/lesson3/task3/index.js
+++ JavaScript/lesson3/task3/index.js
@@ -28,11 +28,11 @@ false === 0; console.log("false === 0: ", false === 0);
 
 /* нестрогое сравнение */
 
-17 == '17'; console.log("17 == '17': ", 17 === '17');
-'0' == ''; console.log("'0' == '': ", '0' === '');
-0 == ''; console.log("0 == '': ", 0 === '');
-null == undefined; console.log("null == undefined: ", null === undefined);
-false == 0; console.log("false == 0: ", false === 0);
+17 == '17'; console.log("17 == '17': ", 17 == '17');
+'0' == ''; console.log("'0' == '': ", '0' == '');
+0 == ''; console.log("0 == '': ", 0 == '');
+null == undefined; console.log("null == undefined: ", null == undefined);
+false == 0; console.log("false == 0: ", false == 0);
 
 
 /* осторожно c null и undefined */
@@ -41,4 +41,4 @@ undefined == null; console.log("undefined == null: ", undefined == null);
 undefined == 0; console.log("undefined == 0: ", undefined == 0);
 null == 0; console.log("null == 0: ", null == 0);
 undefined < 0; console.log("undefined < 0: ", undefined < 0);
-undefined > 0; console.log("undefined > 0: ", undefined > 0);
+undefined > 0; console.log("undefined > 0: ", undefined < 0);
