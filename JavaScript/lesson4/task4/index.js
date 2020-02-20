7272343a8ad4f957d70ea1b95b5949d3dfcb23d0
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-20 23:02:05 +0200

    Update index.js

--- JavaScript/lesson4/task4/index.js
+++ JavaScript/lesson4/task4/index.js
@@ -1,7 +1,7 @@
-const n = 10;
-let sum = String ();
-for (i = String (1); i <= n; i++) {
+const n = 7;
+let sum = '';
+for (i = 1; i <= n; i++) {
     sum += i;
 }
 
-console.log("result: " + sum);
+console.log(sum);
