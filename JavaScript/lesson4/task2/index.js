19a475d2357f62b3ace5e5550ba839d1b4266e87
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-21 09:56:23 +0200

    Update index.js

--- JavaScript/lesson4/task2/index.js
+++ JavaScript/lesson4/task2/index.js
@@ -1,9 +1,9 @@
 const m = 1;
 const n = 10;
-let result = 0;
+let result = 1;
 for (let i = m; i <= n; i++) {
     if (i % 2 === 1) {
-        result += i;
+        result *= i;
     }
-    console.log("Result: " + result);
 }
+    console.log("Result: " + result);
