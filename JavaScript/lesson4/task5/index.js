5a82a5ff4b4fe3d965a47a039462024902dc2277
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-21 10:06:25 +0200

    Update index.js

--- JavaScript/lesson4/task5/index.js
+++ JavaScript/lesson4/task5/index.js
@@ -1,45 +1,20 @@
-// const m = 1;
-// const n = 50;
-// let result = 0;
-// for (let i = m; i <= n; i++) {
-//     if (i % 2 === 0) {
-//         result += i;
-//     }
-//     else if (i % 3 === 0) {
-//         result = result - i;
-//     }
-//     else if (i % 4 === 0) {
-//         result = result * i;
-//     }
-//     if (i % 5 === 0) {
-//         console.log(i);
-//     }
-//     else {
-//         continue;
-//     }
-// }
-let m = 0;
-let n = 9;
+const m = 1;
+const n = 50;
 let result = 0;
-
-
-for(let i = m; i <= n; i++){
-    if((i % 2 === 0) && (i % 4 !== 0)){
-        result += i 
-       
+for (let i = m; i <= n; i++) {
+    if (i % 2 === 0) && (i % 4 !== 0)) {
+        result += i;
     }
-    else if(i % 3 === 0){
-        result -= i
-        
+    else if (i % 3 === 0) {
+        result = result - i;
     }
-    else if(i % 4 === 0){
-        result *= i 
-      
+    else if (i % 4 === 0) {
+        result = result * i;
     }
     if (i % 5 === 0) {
         console.log(i);
     }
-
-
+    else {
+        continue;
+    }
 }
-
