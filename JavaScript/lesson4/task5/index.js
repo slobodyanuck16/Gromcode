dc6c5e346d9e11fb7f02b483e3e21429fdac1359
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-21 10:09:20 +0200

    Update index.js

--- JavaScript/lesson4/task5/index.js
+++ JavaScript/lesson4/task5/index.js
@@ -2,7 +2,7 @@ let m = 0;
 let n = 50;
 let result = 0;
 for (let i = m; i <= n; i++) {
-    if (i % 2 === 0) && (i % 4 !== 0)) {
+    if (i % 2 === 0) && (i % 4 !== 0) {
         result += i;
     }
     else if (i % 3 === 0) {
