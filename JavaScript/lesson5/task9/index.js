83597267b4ecebfb16e88b62b2722d3650ae02c7
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-23 01:10:54 +0200

    commit

--- JavaScript/lesson5/task9/index.js
+++ JavaScript/lesson5/task9/index.js
@@ -4,7 +4,22 @@ function findDivCount(a,b,n) {
         if (a % n === 0) {
             quan = quan + 1;
         }
+        // else {
+        //     return(0);
+        // }
         a++;
     } while (a<=b);
-    console.log(quan);
+    return(quan);
 }
+// function findDivCount(a,b,n) {
+//     for (let quan = 0; a <= b; a++) {
+//         if (a % n === 0) {
+//             quan = quan + 1;
+//             console.log(quan);
+            
+//         }
+        // else {
+        //     return (0);
+        // }
+//     }
+// }
