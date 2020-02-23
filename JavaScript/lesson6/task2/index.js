4b76d64f77f79a761cc79ee7f7c15ff77435b700
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-23 22:21:51 +0200

    commit

--- JavaScript/lesson6/task2/index.js
+++ JavaScript/lesson6/task2/index.js
@@ -1,11 +1,13 @@
-const numbers = [1,2,3,4,5,6]
-function getSum (numbers) {
+const numbers = [1, 2, 3, 4, 5, 6]
+
+function getSum(numbers) {
     let sumOfNumbers = 0;
-    for(let i= 0; i < numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         sumOfNumbers += numbers[i];
+    if (!Array.isArray(numbers)) {
+            return null;
+        }
     }
     return sumOfNumbers;
-    if (!Array.isArray(numbers)) {
-        return null;
-    }
+
 }
