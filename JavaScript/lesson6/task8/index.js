e2566467b5461d04eb591c75f016adcb48bd32e7
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-24 10:38:41 +0200

    commit

--- JavaScript/lesson6/task8/index.js
+++ JavaScript/lesson6/task8/index.js
@@ -1,5 +1,6 @@
 function uniqueCount(array) {
     let uniqArray = [];
+    let count = 0;
     for (let i = 0; i < array.length; i++) {
         let uniq = false;
         for (let j = 0; j < uniqArray.length; j++) {
@@ -9,10 +10,11 @@ function uniqueCount(array) {
         }
         if (uniq == false) {
             uniqArray.push(array[i]);
+            count++;
         }
     }
-    console.log(uniqArray);
+    console.log(uniqArray, count);
 
-    return uniqArray;
+    return count;
 }
 console.log(uniqueCount([1,1,1,1,1,2,2,2,2,2,3,3,33,4,4,4,4,5,5,5,6,6]))
