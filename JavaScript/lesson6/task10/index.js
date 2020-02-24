7c62255bd04ed4fcc068715e76bff743fc5af44e
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-24 13:48:22 +0200

    commit

---
+++ JavaScript/lesson6/task10/index.js
@@ -0,0 +1,11 @@
+function withdraw(clients, balances, client, amount) {
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) {
+            if (balances[i] > amount) {
+                return balances[i] -= amount
+            } else {
+                return -1
+            }
+        }
+    }
+}
