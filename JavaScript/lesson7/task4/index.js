3f576abcb0f8c7f18ae66f7f70ed7ff34a49ffd8
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-25 07:47:08 +0200

    commit

--- JavaScript/lesson7/task4/index.js
+++ JavaScript/lesson7/task4/index.js
@@ -1,6 +1,9 @@
-const getMessagesForBestStudents = (allStudents, failedStudents) => {
+function getMessagesForBestStudents (allStudents, failedStudents) {
     const specialStudents = allStudents
-        .filter(name => !failedStudents.include(name))
+        .filter(name => !(failedStudents.indexOf(name) !== -1))
         .map(name => 'Good job, ' + name);
         return specialStudents;
 };
+const allStudents = ['Ann','Tom','Bob','Kate'];
+const failedStudents = ['Tom','Bob'];
+console.log(getMessagesForBestStudents(allStudents, failedStudents));
