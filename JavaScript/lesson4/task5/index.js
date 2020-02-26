2251e3d6554e8c92990f3968e11671988afc37b5
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-26 12:28:02 +0200

    commit

--- JavaScript/lesson4/task5/index.js
+++ JavaScript/lesson4/task5/index.js
@@ -14,7 +14,4 @@ for (let i = m; i <= n; i++) {
     if (i % 5 === 0) {
         console.log(i);
     }
-    else {
-        continue;
-    }
 }
--- JavaScript/lesson9/task4/index.js
+++ JavaScript/lesson9/task4/index.js
@@ -1,32 +1,24 @@
 const obj = {
-    room1:[
-        {name:'room1 name1'},
-        {name:'room1 name2'},
-        {name:'room1 name3'},
-        {name:'room1 name4'},
+    room1: [
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
     ],
-    room2:[
-        {name:'room2 name1'},
-        {name:'room2 name2'},
-        {name:'room2 name3'},
-        {name:'room2 name4'},
+    room2: [
+        { name: 'room2 name1' },
+        { name: 'room2 name2' },
+        { name: 'room2 name3' },
+        { name: 'room2 name4' },
     ],
-    room3:[
-        {name:'room3 name1'},
-        {name:'room3 name2'},
-        {name:'room3 name3'},
-        {name:'room3 name4'},
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+        { name: 'room3 name4' },
     ],
-
 }
-const transformToObject = obj => {
-    const copy = Object.entries(obj);
-    console.log(obj);
-    
-    copy.forEach(el => {
-        obj[el] = el;
-    });
-    return copy;
+const transformToObject = obj =>
+    Object.values(obj).flat().map(el => el.name);
 
-};
 console.log(transformToObject(obj));
