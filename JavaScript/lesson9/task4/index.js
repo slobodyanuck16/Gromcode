e935a74b038cac8d13dc35be7f39fe74fd62e162
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-26 11:13:44 +0200

    commit

---
+++ JavaScript/lesson9/task4/index.js
@@ -0,0 +1,32 @@
+const obj = {
+    room1:[
+        {name:'room1 name1'},
+        {name:'room1 name2'},
+        {name:'room1 name3'},
+        {name:'room1 name4'},
+    ],
+    room2:[
+        {name:'room2 name1'},
+        {name:'room2 name2'},
+        {name:'room2 name3'},
+        {name:'room2 name4'},
+    ],
+    room3:[
+        {name:'room3 name1'},
+        {name:'room3 name2'},
+        {name:'room3 name3'},
+        {name:'room3 name4'},
+    ],
+
+}
+const transformToObject = obj => {
+    const copy = Object.entries(obj);
+    console.log(obj);
+    
+    copy.forEach(el => {
+        obj[el] = el;
+    });
+    return copy;
+
+};
+console.log(transformToObject(obj));
