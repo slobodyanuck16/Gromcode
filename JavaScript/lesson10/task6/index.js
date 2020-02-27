0e05bde90b6c9faf71776597f4c86dc04f15d862
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-27 07:50:26 +0200

    Update index.js

--- JavaScript/lesson10/task6/index.js
+++ JavaScript/lesson10/task6/index.js
@@ -3,8 +3,8 @@ let start = 1;
 let end = 20;
 
 const getRandomNumbers = (lenght, start, end) => {
-    // const wrongNum = (start < end) && Math.ceil(end) === Math.ceil(start);
-    if (start > end) return null;
+    const wrongNum = (start < end) && Math.ceil(end) === Math.ceil(start);
+    if (start > end || wrongNum) return null;
     return Array(lenght)
         .fill().map(el => Math.trunc(Math.random() * lenght));
 }
