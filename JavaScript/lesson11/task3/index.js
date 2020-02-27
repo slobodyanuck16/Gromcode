81a1fece10f76c3e03c251277c448c59f8876608
slobodyanuck16 <60445099+slobodyanuck16@users.noreply.github.com>
2020-02-27 19:28:05 +0200

    Update index.js

--- JavaScript/lesson11/task3/index.js
+++ JavaScript/lesson11/task3/index.js
@@ -1,25 +1,44 @@
-const splitString = (text, len) => {
-    const strArr = [];
+// const splitString = (text, len) => {
+//     const strArr = [];
     
-    if (text === !(String)) {
-        return null;
-    }
+//     if (text === !(String)) {
+//         return null;
+//     }
+//     let startPosition = 0;
+//     while (true) {
+
+//         let chunk = text.substr(startPosition, len);
+//         if (chunk.length === 0) {
+//             break;
+//         }
+//         if (len === undefined) {
+//             strArr.push(chunk.slice(0));
+//             startPosition += 10;
+//         } else {
+//             strArr.push(chunk.slice(0));
+//             startPosition += len;
+//         }
+//     }
+
+//     return strArr;
+// }
+// console.log(splitString('asvsadsafsagsagsafsa',4));
+const splitString = (str, num = 10) => {
+
+    if (typeof str !== 'string') return null;
+
+    const resultArray = [];
     let startPosition = 0;
-    while (true) {
 
-        let chunk = text.substr(startPosition, len);
-        if (chunk.length === 0) {
-            break;
+    while (true) {
+        let chank = str.substr(startPosition, num);
+        if (chank.length === 0) break;
+        if (chank.length < num) {
+            chank = chank + '.'.repeat(num - chank.length);
         }
-        if (len === undefined) {
-            strArr.push(chunk.slice(0));
-            startPosition += 10;
-        } else {
-            strArr.push(chunk.slice(0));
-            startPosition += len;
-        }
+        resultArray.push(chank);
+        startPosition += num;
     }
 
-    return strArr;
+    return resultArray;
 }
-console.log(splitString('asvsadsafsagsagsafsa',4));
