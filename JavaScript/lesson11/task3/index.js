2ce72413ca32647122816bfdb9aa02429a5512ff
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-27 19:03:54 +0200

    commit

---
+++ JavaScript/lesson11/task3/index.js
@@ -0,0 +1,24 @@
+const splitString = (text, len) => {
+    const strArr = [];
+    if (text === !(String)) {
+        return null;
+    }
+    let startPosition = 0;
+    while (true) {
+
+        let chunk = text.substr(startPosition, len);
+        if (chunk.length === 0) {
+            break;
+        }
+        if (len === undefined) {
+            strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+            startPosition += 10;
+        } else {
+            strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+            startPosition += len;
+        }
+
+    }
+    return strArr.join('.\n');
+}
+console.log(splitString('21312412412512512522222222222',4));
