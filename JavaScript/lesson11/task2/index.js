450ba6c84b4d80b009f4195637910850b33caf10
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-27 18:54:11 +0200

    commit

--- JavaScript/lesson11/task2/index.js
+++ JavaScript/lesson11/task2/index.js
@@ -1,7 +1,34 @@
-let text = '1234'
-const splitText = (text, len) => { 
-    if (len === undefined) {
-        let len = 10;
+let bool = true;
+let contacts = [{
+        name: 'Adel',
+        phoneNumber: '111-11-11',
+    },
+    {
+        name: 'Vlad',
+        phoneNumber: '222-222-222',
+    },
+    {
+        name: 'Nikita',
+        phoneNumber: '77-77-77',
+    },
+    {
+        name: 'Artem',
+        phoneNumber: '13-13-13',
+    },
+]
+
+const sortContacts = (contacts, bool) => {
+    const result = contacts
+    if (bool == true) {
+        contacts.sort((a, b) => {
+            return a.name.localeCompare(b.name);
+        });
+    } else {
+        contacts.sort((a, b) => {
+            return b.name.localeCompare(a.name);
+        });
+
     }
-    return len;
+    return result;
 }
+console.log(sortContacts(contacts, bool));
