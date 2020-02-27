2b013d05aaa8e4ab0fea3255d352ed348941087c
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-02-27 08:36:48 +0200

    commit

--- JavaScript/lesson10/task6/index.js
+++ JavaScript/lesson10/task6/index.js
@@ -3,8 +3,8 @@ let start = 1;
 let end = 20;
 
 const getRandomNumbers = (lenght, start, end) => {
-    // const wrongNum = (start < end) && Math.ceil(end) === Math.ceil(start);
-    if (start > end) return null;
-    return Array(lenght)
-        .fill().map(el => Math.trunc(Math.random() * lenght));
-}
+    const wrongNum = (start < end) && Math.ceil(end) === Math.ceil(start);
+    if (start > end || wrongNum) return null;
+    return Array(length).fill().map(num =>
+        Math.floor(Math.random() * (start - end + 1) + end));
+};
--- JavaScript/lesson10/task7/index.js
+++ JavaScript/lesson10/task7/index.js
@@ -1,3 +1,8 @@
-const getAdults = usersObj => Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
+function superRound (num,round) {
+    let arr = [Math.floor(num * (round * Math.pow(10, round))) / (round * Math.pow(10, round)),
+    Math.round(num * (round * Math.pow(10, round))) / (round * Math.pow(10, round)),
+    Math.ceil(num * (round * Math.pow(10, round))) / (round * Math.pow(10, round)),
+    Math.trunc(num * (round * Math.pow(10, round))) / (round * Math.pow(10, round)),
+    num.toFixed(round)]
+    return arr;
+};
