2c808339337358c1b2eb471460311b36208ca9f3
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-02 22:40:31 +0200

    commit

---
+++ JavaScript/lesson14/task1/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let message = 'Just learn it!';
+
+function sendMessage(name) {
+    const sender = 'Gromcode';
+
+    console.log(`${name}, ${message}, Your ${sender}`);
+}
+
+function setMessage(text) {
+    message = text;
+}
+
+sendMessage('Ann');
+
+setMessage('Good job!');
+
+sendMessage('Ann');
