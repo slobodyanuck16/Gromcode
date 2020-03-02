2efa7b5e8ea44b428d18357958dda0a50c77babe
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-02 22:44:47 +0200

    commit

--- JavaScript/lesson14/task1/index.js
+++ JavaScript/lesson14/task1/index.js
@@ -2,13 +2,13 @@
 
 let message = 'Just learn it!';
 
-function sendMessage(name) {
+export function sendMessage(name) {
     const sender = 'Gromcode';
 
-    console.log(`${name}, ${message}, Your ${sender}`);
+    console.log(`${name}, ${message} Your ${sender}`);
 }
 
-function setMessage(text) {
+export function setMessage(text) {
     message = text;
 }
 
