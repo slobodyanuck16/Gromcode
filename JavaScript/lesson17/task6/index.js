53656d03e9b6513c787d78f2fb145e4451f7efdf
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-05 15:38:09 +0200

    commit

--- JavaScript/lesson17/task6/index.js
+++ JavaScript/lesson17/task6/index.js
@@ -1,5 +1,5 @@
 function bind(func, context) {
-    let bindArgs = [].slice.call(arguments);
+    let bindArgs = [].slice.call(arguments, 2);
     return function() {
         let funcArgs = [].slice.call(arguments)
         return func.apply(context, bindArgs.concat(funcArgs));
