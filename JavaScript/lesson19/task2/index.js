1d89461341ad68f598e1eed8248fa42ced531296
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-08 17:59:05 +0200

    commit

--- JavaScript/lesson19/task2/index.js
+++ JavaScript/lesson19/task2/index.js
@@ -1,6 +1,15 @@
+// function getOwnProps (obj) {
+//     const arr = [];
+//     arr.push(Object.keys(obj));
+//     return arr;
+// }
 function getOwnProps (obj) {
     const arr = [];
-    arr.push(Object.keys(obj));
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
+            arr.push(prop)
+        }
+    }
     return arr;
 }
 
