01775af700d7e7c16327f940e88107453b0ab755
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-10 15:42:51 +0200

    commit

---
+++ JavaScript/lesson20/task5/index.js
@@ -0,0 +1,42 @@
+class User {
+    constructor(id, name, sessionId) {
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
+    }
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get sessionId() {
+        return this._sessionId;
+    }
+}
+
+class UserRepository {
+    constructor(users) {
+        this._users = Object.freeze(users);
+    }
+    get users() {
+        return this._users;
+    }
+    getUserNames() {
+        return this._users.map(item => item._name);
+    }
+
+    getUserIds() {
+        return this._users.map(item => item._id);
+    }
+
+    getUserNameById(id) {
+        for (let user of this._users) {
+            if (user.id === id) {
+                return user.name;
+            }
+        }
+    }
+}
