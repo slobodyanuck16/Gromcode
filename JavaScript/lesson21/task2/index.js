ffcec5f49a1fc6d9bb974512c34f35e88ab4aa22
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-11 19:35:37 +0200

    commit

---
+++ JavaScript/lesson21/task2/index.js
@@ -0,0 +1,12 @@
+function getTitleElement () {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+}
+function getInputElement () {
+    const inputElem = document.querySelector('input', 'text');
+    console.dir(inputElem);
+    return inputElem;
+}
+
+export default { getTitleElement, getInputElement }
