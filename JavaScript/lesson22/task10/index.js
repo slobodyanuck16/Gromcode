0c1dc6d9944a60493d0e693b1042e735cfe3da11
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-16 14:11:07 +0200

    cm

--- JavaScript/lesson22/task10/index.js
+++ JavaScript/lesson22/task10/index.js
@@ -1,5 +1,5 @@
 const pages = document.querySelectorAll('.pagination__page');
-const handleClick = () => console.log(pages.dataset.dataPageNumber);
-const onePage = pages.forEach(page => {
+const handleClick = (event) => console.log(event.target.dataset.pageNumber);
+pages.forEach(page => {
     page.addEventListener('click', handleClick);
 });
