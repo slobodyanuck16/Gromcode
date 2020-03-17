6fba98d9cc3cb5a0c87ec927fd3c57503bf095ac
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-17 10:52:42 +0200

    cm

--- JavaScript/lesson24/task2/index.js
+++ JavaScript/lesson24/task2/index.js
@@ -1,10 +1,10 @@
-const getGreenwichTime = new Intl.DateTimeFormat('en', {
+const formatter = new Intl.DateTimeFormat('en', {
     timeZone: 'GMT',
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
 });
 
-const getGreenwichTime = date => getGreenwichTime.format(date);
+const getGreenwichTime = date => formatter.format(date);
 
 export {getGreenwichTime}
