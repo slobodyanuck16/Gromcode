649e5959b2843095908c0acda4fa190dd48dc65e
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-08 23:10:07 +0300

    cm

--- JavaScript/lesson31/task1/index.js
+++ JavaScript/lesson31/task1/index.js
@@ -1,17 +1,19 @@
 export const requestUserData = userId => {
-  const request = new Promise((resolve, reject) => {
-    if (userId === 'broken') {
-      setTimeout(new Error('User not found'), 500);
-    } else {
-      setTimeout(() => {
-        resolve({
-          name: 'John',
-          age: 17,
-          userId: `${userId}`,
-          email: `${userId}@example.com`,
-        });
-      }, 1000);
-    }
-  });
-  return request;
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    userId: `${userId}`,
+                    email: `${userId}@example.com`,
+                });
+            }, 1000);
+        }
+    });
+    return request;
 };
