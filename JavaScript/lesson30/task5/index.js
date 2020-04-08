f6b2b3ab1fbafa937351fa90af2a3a1747a4a8f1
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-08 20:44:53 +0300

    cm

---
+++ JavaScript/lesson30/task5/index.js
@@ -0,0 +1,30 @@
+export const addImageV2 = imgSrc => {
+  const addImage = new Promise ((resolveCb, rejectCb) => {
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'User avatar');
+    img.src = imgSrc;
+  
+    const pageElem = document.querySelector('.page');
+    pageElem.append(img);
+    
+    const onImageLoaded = () => {
+        const { width, height } = img;
+        resolveCb({ width, height });
+    }
+    
+    const onImageLoadError = () => rejectCb('Image load failed');
+    
+    img.addEventListener('load', onImageLoaded);
+    
+    img.addEventListener('error', onImageLoadError);
+  });
+  return addImage;
+}
+
+const imgSrc = 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg';
+
+const result = addImageV2(imgSrc);
+
+result.then(data => console.log(data));
+
+console.log(result);
