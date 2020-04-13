e4a5a473668798ea54bc9c1418fa0bd718d2cb4f
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-13 13:54:34 +0300

    cm

---
+++ JavaScript/lesson32/task5/index.js
@@ -0,0 +1,13 @@
+export const delay = time => new Promise((resolve) => {
+  setTimeout(() => {
+      resolve(delay);
+  }, time)
+})
+// export const delay = time => new Promise((resolve) => {
+//       resolve(setTimeout(() => {
+
+//       }),time);
+// })
+
+delay(3000)
+.then(() => (console.log('Done')));
