1cc2aa9f0a4b8d5a346bcdb6405f9f848c970b9a
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-14 13:24:39 +0300

    c

--- JavaScript/lesson33/task2/index.js
+++ JavaScript/lesson33/task2/index.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
 
-const getTasksList = () => {
+export const getTasksList = () => {
     return fetch(baseUrl)
         .then(response =>
             response.json());
@@ -13,7 +13,7 @@ console.log(getTasksList());
 
 /* getTaskById code here */
 
-const getTaskById = (id) => {
+export const getTaskById = (id) => {
     return fetch(baseUrl)
     .then(response => response.json())
     .then(arr => arr[+`${id}` - 1])
