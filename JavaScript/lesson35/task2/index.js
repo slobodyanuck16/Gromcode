7551ef72ce9c7a64240f98eb5bd3b67f288e760e
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-21 10:06:49 +0300

    c

---
+++ JavaScript/lesson35/task2/index.js
@@ -0,0 +1,48 @@
+const baseUrl = 'https://5e97f8bc77f5430016339cb5.mockapi.io/api/v1/users';
+
+/* getUsersList code here */
+
+export const getUsersList = () => {
+  return fetch(baseUrl)
+      .then(response => response.json());
+}
+
+/* getUserById code here */
+
+export const getUserById = userId => {
+  return fetch(`${baseUrl}/${userId}`)
+  .then(response => response.json())
+}
+
+
+/* createUser code here */
+
+export const createUser = userObj => {
+  return fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(userObj),
+  })
+}
+
+/* updateUser code here */
+
+export const updateUser = (userId, updatedUserData) => {
+  return fetch(`${baseUrl}/${userId}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(updatedUserData),
+  })
+}
+
+/* deleteUser code here */
+
+export const deleteUser = userId => {
+  return fetch(`${baseUrl}/${userId}`, {
+      method: 'DELETE',
+  })
+}
