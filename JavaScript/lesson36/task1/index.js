831f4d07bb08c93ebaa66b75abc25bf12597b0ca
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-21 13:26:12 +0300

    c

---
+++ JavaScript/lesson36/task1/index.js
@@ -0,0 +1,17 @@
+const fetchUser = async userId => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userId}`);
+        if(!response.ok) {
+            return null;
+        }
+        const userData = await response.json();
+        return userData;
+    } catch (err) {
+        throw new Error('Failed  to fetch user');
+    }
+};
+
+fetchUser('slobodyanuck16')
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message));
+    
