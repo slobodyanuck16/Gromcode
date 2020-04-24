57442157c018f9a7a8b451fcdf0ef287ede34562
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-24 13:37:24 +0300

    c

--- FE-Tools/lesson7/task2/webpack.config.js
+++ FE-Tools/lesson7/task2/webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -9,7 +14,7 @@ module.exports = {
                 use: ['babel-loader']
             }, {
                 test: /.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /.jpg|png$/,
@@ -23,5 +28,15 @@ module.exports = {
                 }, ],
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
 }
